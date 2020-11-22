04ad3dcd840fd64446d8ed4cc2e7ef1a1e019e85
Ryuichi Watanabe <ryucrosskey@gmail.com>
2020-11-22 13:31:41 +0900

    Create install-s3cmd.sh

---
+++ install-s3cmd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -rp "Install and Configure s3cmd? (y/n) " INSTALL
+if [[ $INSTALL =~ ^([yY][eE][sS]|[yY])$ ]]; then
+
+	# Test if already installed
+	command -v s3cmd >/dev/null 2>&1 || {
+		cd ~
+		git clone https://github.com/s3tools/s3cmd.git
+		cd s3cmd
+		sudo python setup.py install
+		read -rp "Configure s3cmd? (y/n) " CONFIGURE
+			if [[ $CONFIGURE =~ ^([yY][eE][sS]|[yY])$ ]]; then
+				s3cmd --configure
+			fi
+	}
+	echo
+	echo "Completed."
+fi
